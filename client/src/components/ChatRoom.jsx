b5ac61488716a1575f1d36149bc9461fb47ac646
Johnny <po_bo@hotmail.com>
2017-02-08 15:48:37 -0800

    implemented chat box to work and append message

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -1,34 +1,71 @@
 const React = require('react');
+const io = require('socket.io-client')
 
 class ChatRoom extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      message: 'GOOD MORNING!!'
+      messages: [],
+      message: ''
     };
 
-    this.messageHandler = this.messageHandler.bind(this);
+    // this.messageHandler = this.messageHandler.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
   }
 
-  messageHandler (event) {
-    event.preventDefault();
-    this.setState({
-      message: event.target.value
-    });
+  // messageHandler (event) {
+  //   this.setState({
+  //     message: event.target.value
+  //   });
+  //   console.log(this.state.message)
+  // }
+
+  componentWillMount() {
+    this.socket = io('/');
+    this.socket.on('message', function(message) {
+      this.setState({
+        messages: [message, ...this.state.messages]
+      });
+    })
+  }
+
+  handleEnter (event) {
+    event.preventDefault()
+    const body = event.target.value;
+    const user = this.props.username;
+    if (event.keyCode === 13) {
+      const message = {
+        body,
+        user
+      }
+
+      this.setState({
+        messages: [message, ...this.state.messages]
+      });
+      console.log('enter pressed')
+    event.target.value = '';
+    }
     console.log(this.state.message)
   }
 
   render() {
-    console.log(this.props.username)
+    //console.log(this.props.username)
     return(
       <div className="container-fluid">
       <h3>Chat Room</h3>
-        <ul id="messages"></ul>
-          <form action="">
+        <ul id="messages">
+          {this.state.messages.map((message, index) => {
+            return <li key={index}>
+              <b>{message.user}: </b>
+              {message.body}
+            </li>
+          })}
+        </ul>
+          <form className="fixed-bottom" action="">
             <label>Discuss your thoughts:</label><br />
               <span className="input-group">
-                <input id="message" className="form-control" type="text" placeholder="Enter Message" onChange={this.messageHandler} autoComplete="off" />
+                <input id="message" className="form-control" type="text" placeholder="Enter Message" onKeyUp={this.handleEnter} autoComplete="off" />
                   <span className="input-group-btn">
                   <button type="submit" className="btn btn-default">Send</button>
                   </span>
--- server/server.js
+++ server/server.js
@@ -3,14 +3,25 @@ var mongoose = require('mongoose');
 //MONGODB CONNECTION
 mongoose.connect('mongodb://localhost/billfetchertest');
 
+var socketIo = require('socket.io');
 var http = require('http');
 var app = require('./server-config.js');
+
+var io = socketIo(server);
 
 
 //HTTP SERVER CONNECTION
 var server = http.createServer(app);
 var port = process.env.PORT || 8080;
 
+io.on('connection', function(socket) {
+  socket.on('message', function(body) {
+    socket.broadcast.emit('message', {
+      body: body
+      user: socket.id.slice(8);
+    })
+  })
+})
 
 server.listen(port, function() {
   console.log('Server now listening on port ' + port);
