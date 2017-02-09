bc4bb5560af85edc9177d9169a5e2dd8f57fa49a
Isaac Yoon <ysaacioon@yahoo.com>
2017-02-08 17:46:32 -0800

    appends imgs and title that are responsive

--- client/src/data/googTrends.js
+++ client/src/data/googTrends.js
@@ -7,17 +7,19 @@ var queryGoogle = (query, callback) => {
 	var params = {
 		geo: 'US',
 		// date:
-		keywords: query,
+		keywords: ['politics','trump', query],
 		category: 'politics'
 	}
 
-	googleTrends.hotTrendsDetail(options)
+	googleTrends.hotTrendsDetail(params)
 	.then(function(result){
-		console.log(result);
-		callback(result);
+		callback(result.rss.channel[0].item);
+		// console.log(result.rss.channel[0].item);
+		// callback(result);
 	}).catch(function(err){
 		console.log('error detected', err);
 	})
+
 }
 
 export default queryGoogle;
