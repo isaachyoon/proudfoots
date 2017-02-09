28a48c8b065ce953158d7d1a66e96b2e9cdb7ecf
Johnny <po_bo@hotmail.com>
2017-02-09 14:33:17 -0800

    implemented chat function and fixed bugs on double entery on submit and got message receive on front end and showing

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -10,47 +10,68 @@ class ChatRoom extends React.Component {
       message: ''
     };
 
-    // this.messageHandler = this.messageHandler.bind(this);
-    this.handleEnter = this.handleEnter.bind(this);
+    //bind all handlers to this context.
+    this.textHandler = this.textHandler.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // messageHandler (event) {
-  //   this.setState({
-  //     message: event.target.value
-  //   });
-  //   console.log(this.state.message)
-  // }
+  //handle text area change and set to message state.
+  textHandler (event) {
+    this.setState({
+      message: event.target.value
+    });
+  }
 
-  componentWillMount() {
+  //for all socket.io events need to handle by componentDidMount.
+  componentDidMount() {
+    const context = this;
+    //socket.io default path is '/' but can also manually define path ('/').
     this.socket = io('/');
     this.socket.on('message', function(message) {
-      this.setState({
-        messages: [message, ...this.state.messages]
-      });
+      console.log('message received at front end', message)
+      context.setState({
+        //this method will insert new message to the first of the message array and the spread operator will set the rest of the messages after the new message.
+        messages: [...context.state.messages, message]
+      })
+      console.log("this thing", context.state.messages)
     })
   }
 
-  handleEnter (event) {
-    event.preventDefault()
-    const body = event.target.value;
-    const user = this.props.username;
-    if (event.keyCode === 13) {
-      const message = {
-        body,
-        user
-      }
-
+  //check if Enter key is press or mouse click
+  handleSubmit(event) {
+    if (event.key === 'Enter') {
+      this.handleMessage(event);
       this.setState({
-        messages: [message, ...this.state.messages]
+        message: ''
       });
-      console.log('enter pressed')
-    event.target.value = '';
+    } else if (event.button === 0) {
+      this.handleMessage(event);
+      this.setState({
+        message: ''
+      })
     }
-    console.log(this.state.message)
+  }
+
+  //this method will create the message object with message and user name then socket.oi will emit the message object to the server socket.
+  handleMessage (event) {
+    event.preventDefault()
+    const body = this.state.message;
+    const user = this.props.username;
+    const message = {
+      body,
+      user
+    }
+
+    this.setState({
+      messages: [...this.state.messages, message]
+    });
+
+    this.socket.emit('message', message)
+    console.log("message sent from front end")
   }
 
   render() {
-    //console.log(this.props.username)
     return(
       <div className="container-fluid">
       <h3>Chat Room</h3>
@@ -62,15 +83,17 @@ class ChatRoom extends React.Component {
             </li>
           })}
         </ul>
-          <form className="fixed-bottom" action="">
+          <footer className="navbar-fixed-bottom" style={{width: 810, marginLeft: 450, marginBottom: 20}}>
+          <form action="">
             <label>Discuss your thoughts:</label><br />
               <span className="input-group">
-                <input id="message" className="form-control" type="text" placeholder="Enter Message" onKeyUp={this.handleEnter} autoComplete="off" />
+                <input id="message" className="form-control" type="text" placeholder="Enter Message" value={this.state.message} onChange={this.textHandler} onKeyDown={this.handleSubmit} autoComplete="off" />
                   <span className="input-group-btn">
-                  <button type="submit" className="btn btn-default">Send</button>
+                  <button className="btn btn-default" onClick={this.handleSubmit} >Send</button>
                   </span>
               </span>
           </form>
+          </footer>
       </div>
     );
   }
--- server/server.js
+++ server/server.js
@@ -2,27 +2,31 @@
 var mongoose = require('mongoose');
 //MONGODB CONNECTION
 mongoose.connect('mongodb://localhost/billfetchertest');
-
+//import socket.io and http because socket.io rely on http to connect.
 var socketIo = require('socket.io');
 var http = require('http');
 var app = require('./server-config.js');
 
-var io = socketIo(server);
 
 
 //HTTP SERVER CONNECTION
-var server = http.createServer(app);
 var port = process.env.PORT || 8080;
+//create server with http (because socket require connection with http not just app) then pass in app so all handlers will be part of http server.
+var server = http.createServer(app);
+//have socket.io pair with http server.
+var io = socketIo(server);
 
+//socket.oi connection configuration for the server end to handle incomming data with io.on and handle out going data by broadcasting with socket.broadcast.emit.
 io.on('connection', function(socket) {
-  socket.on('message', function(body) {
-    socket.broadcast.emit('message', {
-      body: body
-      user: socket.id.slice(8);
-    })
+  console.log("SOCKET CONNECTED!!!!!")
+  socket.on('message', function(messageObj) {
+    console.log("message received from backend!!", messageObj)
+    socket.broadcast.emit('message', messageObj)
+    console.log("MESSAGE EMITTED")
   })
 })
 
+//have http server listen to port.
 server.listen(port, function() {
   console.log('Server now listening on port ' + port);
 });
